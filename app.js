9a9c3764ffbeadba5ab69758b585667b1fb5b58a
Ryan Tuller <rtuller@bridgehealth.com>
2017-07-31 13:59:08 -0600

    changing app over to node, created initial node package json and entry app.js file

---
+++ app.js
@@ -0,0 +1,9 @@
+// Dependencies
+
+var http = require('http');
+
+http.createServer(function (req, res){
+	res.writeHead(200, {'Content-type' : 'text/plain'});
+	res.end('Hello World');
+}).listen(3000);
+
